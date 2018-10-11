dc6c8bb5308520bf133ff3329077a55dc51eb9af
Shane Whitehead <obishiro@gmail.com>
2018-10-11 19:24:41 +1100

    Switch to a builder pattern

--- HttpEngineCore/HttpEngine.swift
+++ HttpEngineCore/HttpEngine.swift
@@ -14,48 +14,14 @@ public typealias HttpProgressMonitor = (Double) -> Void
 public protocol HttpEngine {
 	
 	// MARK: get
-	func get(from request: String) -> Promise<Data?>
-	func get(from url: URL) -> Promise<Data?>
-
-	func get(from request: String, parameters: [String: String]?) -> Promise<Data?>
-	func get(from url: URL, parameters: [String: String]?) -> Promise<Data?>
-
-	func get(from request: String, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
-	func get(from url: URL, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
-	
-	func get(from request: String, parameters: [String: String]?, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
-	func get(from url: URL, parameters: [String: String]?, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
+	func get() -> Promise<Data?>
 
 	// MARK: put
-	func put(from request: String) -> Promise<Data?>
-	func put(from url: URL) -> Promise<Data?>
-
-	func put(from request: String, parameters: [String: String]?) -> Promise<Data?>
-	func put(from url: URL, parameters: [String: String]?) -> Promise<Data?>
-
-	func put(from request: String, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
-	func put(from url: URL, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
-	
-	func put(from request: String, parameters: [String: String]?, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
-	func put(from url: URL, parameters: [String: String]?, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
+	func put() -> Promise<Data?>
 
 	// MARK: post
-	func post(data: Data, to request: String) -> Promise<Data?>
-	func post(data: Data, to url: URL) -> Promise<Data?>
-
-	func post(data: Data, to request: String, parameters: [String: String]?) -> Promise<Data?>
-	func post(data: Data, to url: URL, parameters: [String: String]?) -> Promise<Data?>
-
-	func post(data: Data, to request: String, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
-	func post(data: Data, to url: URL, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
-	
-	func post(data: Data, to request: String, parameters: [String: String]?, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
-	func post(data: Data, to url: URL, parameters: [String: String]?, progressMonitor: HttpProgressMonitor?) -> Promise<Data?>
+	func post(data: Data) -> Promise<Data?>
 
 	// MARK: delete
-	func delete(from request: String) -> Promise<Data?>
-	func delete(from url: URL) -> Promise<Data?>
-
-	func delete(from request: String, parameters: [String: String]?) -> Promise<Data?>
-	func delete(from url: URL, parameters: [String: String]?) -> Promise<Data?>
+	func delete() -> Promise<Data?>
 }
---
+++ HttpEngineCore/HttpRequestBuilder.swift
@@ -0,0 +1,74 @@
+//
+//  HttpRequestBuilder.swift
+//  HttpEngineCore
+//
+//  Created by Shane Whitehead on 11/10/18.
+//  Copyright © 2018 KaiZen. All rights reserved.
+//
+
+import Foundation
+import Hydra
+
+public typealias ProgressMonitor = (Double) -> Void
+
+public protocol Credentials {
+	var userName: String { get }
+	var password: String { get }
+}
+
+/*
+Credtinals are a pain.  Different systems need them injected into the solution at
+different times in different ways.  Rather then trying to spend to much time
+trying to solve "all" of them, the API provides a optional solution to
+supply some basic credential information which the implementation
+can choose how to apply them.
+
+Further, implementations are also free to expand the concept for their own particular
+needs
+*/
+
+public protocol HttpRequestBuilder {
+	// Forced requirement
+	var url: URL { get }
+	
+	func with(parameters: [String: String]) -> Self
+	func with(header: [String: String]) -> Self
+	func with(credentials: Credentials) -> Self
+	func with(progressMonitor: @escaping ProgressMonitor) -> Self
+}
+
+open class BaseHttpRequestBuilder: HttpRequestBuilder {
+	
+	public let url: URL
+	public var parameters: [String: String]?
+	public var header: [String: String]?
+	public var credentials: Credentials?
+	public var progressMonitor: ProgressMonitor?
+	
+	public init(to: URL) {
+		self.url = to
+	}
+	
+	public func with(parameters: [String : String]) -> Self {
+		self.parameters = parameters
+		return self
+	}
+	
+	public func with(header: [String : String]) -> Self {
+		self.header = header
+		return self
+	}
+	
+	public func with(credentials: Credentials) -> Self {
+		self.credentials = credentials
+		return self
+	}
+	
+	public func with(progressMonitor: @escaping ProgressMonitor) -> Self {
+		self.progressMonitor = progressMonitor
+		return self
+
+	}
+	
+	
+}
