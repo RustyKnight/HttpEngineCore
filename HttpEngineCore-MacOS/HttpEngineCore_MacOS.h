741b2ca22844e50489a620cbfda70151f4d11a4e
Shane Whitehead <obishiro@gmail.com>
2018-12-29 10:59:31 +1100

    Add additional dependencies

---
+++ HttpEngineCore-MacOS/HttpEngineCore_MacOS.h
@@ -0,0 +1,19 @@
+//
+//  HttpEngineCore_MacOS.h
+//  HttpEngineCore-MacOS
+//
+//  Created by Shane Whitehead on 29/12/18.
+//  Copyright © 2018 KaiZen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HttpEngineCore_MacOS.
+FOUNDATION_EXPORT double HttpEngineCore_MacOSVersionNumber;
+
+//! Project version string for HttpEngineCore_MacOS.
+FOUNDATION_EXPORT const unsigned char HttpEngineCore_MacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HttpEngineCore_MacOS/PublicHeader.h>
+
+
---
+++ HttpEngineCore-TVOS/HttpEngineCore_TVOS.h
@@ -0,0 +1,19 @@
+//
+//  HttpEngineCore_TVOS.h
+//  HttpEngineCore-TVOS
+//
+//  Created by Shane Whitehead on 29/12/18.
+//  Copyright © 2018 KaiZen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HttpEngineCore_TVOS.
+FOUNDATION_EXPORT double HttpEngineCore_TVOSVersionNumber;
+
+//! Project version string for HttpEngineCore_TVOS.
+FOUNDATION_EXPORT const unsigned char HttpEngineCore_TVOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HttpEngineCore_TVOS/PublicHeader.h>
+
+
