e7371b9354832862bde0e7340ad50d258a4dbd81
Shane Whitehead <shanew@swann.com>
2019-11-16 19:39:17 +1100

    project updates

---
+++ HTTPEngineCore-macOS/HTTPEngineCore_macOS.h
@@ -0,0 +1,19 @@
+//
+//  HTTPEngineCore_macOS.h
+//  HTTPEngineCore-macOS
+//
+//  Created by Shane Whitehead on 16/11/19.
+//  Copyright © 2019 KaiZen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HTTPEngineCore_macOS.
+FOUNDATION_EXPORT double HTTPEngineCore_macOSVersionNumber;
+
+//! Project version string for HTTPEngineCore_macOS.
+FOUNDATION_EXPORT const unsigned char HTTPEngineCore_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPEngineCore_macOS/PublicHeader.h>
+
+
