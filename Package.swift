b4c627a2f1b7d2d28dccf6e6196515a69ae1512f
Shane Whitehead <shanew@swann.com>
2019-11-17 21:04:05 +1100

    Add support for SPM

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.1
+//
+//  Package.swift
+//  iOS
+//
+//  Created by Shane Whitehead on 17/11/19.
+//  Copyright © 2019 Kaizen Enteripises. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "HttpEngineCore",
+    platforms: [
+			.macOS(.v10_12), .iOS(.v10), .tvOS(.v11), .watchOS(.v4)
+    ],
+    products: [
+        .library(name: "HttpEngineCore", targets: ["HttpEngineCore"]),
+    ],
+    dependencies: [
+        //.package(url: "https://url/of/another/package/named/Utility", from: "1.0.0"),
+			.package(url: "https://github.com/RustyKnight/Cadmus", Package.Dependency.Requirement.branch("feature/spm")),
+			.package(url: "https://github.com/malcommac/Hydra.git", from: "2.0.0"),
+    ],
+    targets: [
+			.target(name: "HttpEngineCore", dependencies: ["Cadmus", "Hydra"], path: "HttpEngineCore"),
+    ]
+)
