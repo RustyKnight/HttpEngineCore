de78612457e3a3f53271d37c034ea2b87eb3f4f8
Shane Whitehead <shanew@swann.com>
2021-05-08 20:34:08 +1000

    XCframeworks - because, fun :/

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #rm -rf ~/Library/Caches/org.carthage.CarthageKit/dependencies/
 # sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer
 xcodebuild -version
-time carthage12 update
+#time carthage12 update
 # sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+
+time carthage update --use-xcframeworks
